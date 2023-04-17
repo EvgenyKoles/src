f577f52f551bcbd35ec632146b250edf52cc6a9b
EvgenyK <parovoz15@yandex.ru>
2023-04-17 22:34:47 +0300

    1.9 general

--- nil_interf/nullinterface.go
+++ nil_interf/nullinterface.go
@@ -4,36 +4,47 @@ import "fmt"
 
 func main() {
 
+	// a := "sklsfkksflsf"
+	// b := "sadfkkasjfkjfaks"
+	// fmt.Println(a, b)
 
-// a := "sklsfkksflsf"
-// b := "sadfkkasjfkjfaks"
-// fmt.Println(a, b)
+	// var name string //console scan
+	// var age int
+	// var name2 string
+	// fmt.Print("Inter your name: ")
+	// fmt.Scan(&name, &name2)
+	// fmt.Print("Inter your age: ")
+	// fmt.Scan(&age)
+	// fmt.Print(name,name2, age)
+
+	// var a string
+	// fmt.Scan(&a)
+	// fmt.Println(string(a[len(a)-2]))
+
+	// var a int
+	// var h int
+	// var m int
+	// fmt.Scan(&a)
+	// h = a/30
+	// m = (a%30)*2
+	// fmt.Print("It is ", h, " hours ", m, " minutes.")
+
+	// var a int = 8
+	// const b int = 10
+	// a = a + b
+	// // b = b + a
+	// fmt.Println(a)
 
 
-// var name string //console scan
-// var age int
-// var name2 string
-// fmt.Print("Inter your name: ")
-// fmt.Scan(&name, &name2)
-// fmt.Print("Inter your age: ")
-// fmt.Scan(&age)
-// fmt.Print(name,name2, age)
+	d := 8 != 5
 
 
-// var a string
-// fmt.Scan(&a)
-// fmt.Println(string(a[len(a)-2]))
 
 
-var a int
-var h int
-var m int
-fmt.Scan(&a)
-
-h = a/30
-m = (a%30)*2
-
-fmt.Print("It is ", h, " hours ", m, " minutes.")
 
 
+
+
+
+	fmt.Println(d)
 }
