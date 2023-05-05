3933c60d3eec5a68a10c5a93a5d0fe128215faf6
EvgenyK <parovoz15@yandex.ru>
2023-05-05 21:51:26 +0300

    Strings

--- nil_interf/nullinterface.go
+++ nil_interf/nullinterface.go
@@ -1,22 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
+	"os"
+	//"strings"
+	"unicode"
+	
 )
 
 func main() {
-    
-fmt.Print(fubonnacci(4))
 
-}
- 
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	rs := []rune(text)
+	
+
+	fmt.Println(string(rs[len(rs)-2])== ".")
+	//fmt.Println(rs[utf8.RuneCountInString(text)-1])
+
+	//strings.HasSuffix(text, string(rs[len(rs)-3]))
+	
+
+	fmt.Println(unicode.IsUpper(rs[0]))
 
 
-
-func fubonnacci(n int) int {
-
-	 var array = []int{}
+	 if string(rs[len(rs)-2]) == "." && unicode.IsUpper(rs[0]) {
+	 fmt.Print("Right")
+	
+	 }else {
+	 	fmt.Print("Wrong")}
 
 
 
