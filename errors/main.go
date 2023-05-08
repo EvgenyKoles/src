57988c7ccc97c751e3454a24969618bccc3c43f1
EvgenyK <parovoz15@yandex.ru>
2023-05-09 01:28:55 +0300

    errors_2

--- errors/main.go
+++ errors/main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
-	//"bufio"
+	//"errors"
 	"fmt"
-	//"unicode"
-	//"os"
-	"strings"
-	//"runtime/trace"
-	//"strings"
-	//	"unicode"
 )
 
-func main() {}
+
+// func main() {
+	
+// 		// package main уже объявлен, нужные импорты уже есть
+// 		var a, b int 
+// 		fmt.Scan (&a, &b)
+// 		result, err := divide(a, b)
+
+// 		if err == nil{
+// 			fmt.Print(result)
+// 		}else{
+// 			fmt.Print("ошибка")
+// 		}
+	
+	
+// 	// errors:= errors.New("my erorr")
+// 	// fmt.Print(input, errors)
+// }
+
+
+// func divide(a int, b int) (int,error) {
+// 	return a / b, nil
+	
+// }
+
+
+//-------------------------- panic
+
+// func main() {
+//     fmt.Println(divide(15, 5))
+//     fmt.Println(divide(4, 0))
+//     fmt.Println("Program has been finished")
+// }
+// func divide(x, y float64) float64{
+//     if y == 0{ 
+//         panic("division by zero!")
+//     }
+//     return x / y
+// }
+
+
+func main() {
+   a := 5
+   a =6
+    fmt.Println(a)
+}
