69822216b87b7bbffa0fa32a2c4aa623e6e718df
Evgeny_l <parovoz15@yandex.ru>
2023-06-18 22:41:27 +0300

    from_lap

--- new_folder/hello.go
+++ new_folder/hello.go
@@ -2,24 +2,102 @@ package main
 
 import (
 	"fmt"
-	//"time"
+	//"math/rand"
+	"sync"
+	"time"
 )
+
+const N = 20
 
 func main() {
 
-	m := make (chan string, 5)
-	
-	task2(m,"8")
-
-	
-}
-
-func task2 (c chan string, line string) {
-
-	for i := 0; i < 5; i++ {
-	c <- line+ " "
-			
+	fn := func(x int) int {
+		//time.Sleep(2 * time.Second)
+		return x * 2
 	}
-	
+	in1 := make(chan int, N)
+	in2 := make(chan int, N)
+	out := make(chan int, N)
+
+	start := time.Now()
+	merge2Channels(fn, in1, in2, out, N+1)
+	for i := 0; i < N+1; i++ {
+		in1 <- i
+		in2 <- i
+	}
+
+	orderFail := false
+	EvenFail := false
+	for i, prev := 0, 0; i < N; i++ {
+		c := <-out
+		if c%2 != 0 {
+			EvenFail = true
+		}
+		if prev >= c && i != 0 {
+			orderFail = true
+		}
+		prev = c
+		fmt.Println(c)
+	}
+	if orderFail {
+		fmt.Println("порядок нарушен")
+	}
+	if EvenFail {
+		fmt.Println("Есть не четные")
+	}
+	duration := time.Since(start)
+	if duration.Seconds() > N {
+		fmt.Println("Время превышено")
+	}
+	fmt.Println("Время выполнения: ", duration)
 }
 
+func merge2Channels(fn func(int) int, in1 <-chan int, in2 <-chan int, out chan<- int, n int) {
+	var (
+		slice1 = make([]int, n)
+		slice2 = make([]int, n)
+		wg     sync.WaitGroup
+		mu     sync.Mutex
+	)
+	wg.Add(n * 4)
+
+	go func() {
+		for i := 0; i < n; i++ {
+			mu.Lock()
+			slice1[i] = <-in1
+			mu.Unlock()
+			wg.Done()
+		}
+	}()
+	go func() {
+		for i := 0; i < n; i++ {
+			mu.Lock()
+			slice2[i] = <-in2
+			mu.Unlock()
+			wg.Done()
+		}
+	}()
+	go func() {
+		for i, v := range slice1 {
+			mu.Lock()
+			slice1[i] = fn(v)
+			mu.Unlock()
+			wg.Done()
+		}
+	}()
+	go func() {
+		for i, v := range slice2 {
+			mu.Lock()
+			slice2[i] = fn(v)
+			mu.Unlock()
+			wg.Done()
+		}
+	}()
+	go func() {
+		defer close(out)
+		wg.Wait()
+		for i := 0; i < n; i++ {
+			out <- slice1[i] + slice2[i]
+		}
+	}()
+}
